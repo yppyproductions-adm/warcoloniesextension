2a2470eec6f0c720683f7a31fd01e440a44678f1
yppyproductions-adm <editorayppy@gmail.com>
2025-12-08 14:46:34 -0300

    Salvando no VS Code 1

--- src/main/java/br/com/warcolonies/core/StrategicTickHandler.java
+++ src/main/java/br/com/warcolonies/core/StrategicTickHandler.java
@@ -4,7 +4,7 @@ import br.com.warcolonies.building.BuildingSimulationManager;
 import br.com.warcolonies.data.WarEconomyManager;
 import br.com.warcolonies.logistics.LogisticsManager;
 import br.com.warcolonies.territory.ConquestManager;
-import net.neoforged.neoforge.event.tick.TickEvent;
+import net.neoforged.neoforge.event.tick.ServerTickEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 
 public class StrategicTickHandler {
@@ -25,10 +25,7 @@ public class StrategicTickHandler {
 
     // Tick estratégico básico para manter o mundo “vivo”
     @SubscribeEvent
-    public void onServerTick(TickEvent.ServerTickEvent event) {
-        if (event.phase != TickEvent.Phase.END) {
-            return;
-        }
+    public void onServerTick(ServerTickEvent.Post event) {
         economyManager.processTick();
         logisticsManager.updateShipments();
         buildingManager.updateTasks();
