fa2791bc2f5461c923efef8330c22e44761c89e1
yppyproductions-adm <editorayppy@gmail.com>
2025-12-08 17:10:23 -0300

    revisando

--- src/main/java/br/com/warcolonies/core/StrategicTickHandler.java
+++ src/main/java/br/com/warcolonies/core/StrategicTickHandler.java
@@ -4,10 +4,11 @@ import br.com.warcolonies.building.BuildingSimulationManager;
 import br.com.warcolonies.data.WarEconomyManager;
 import br.com.warcolonies.logistics.LogisticsManager;
 import br.com.warcolonies.territory.ConquestManager;
-import net.neoforged.neoforge.event.tick.ServerTickEvent;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.tick.ServerTickEvent;
 
 public class StrategicTickHandler {
+
     private final WarEconomyManager economyManager;
     private final LogisticsManager logisticsManager;
     private final BuildingSimulationManager buildingManager;
